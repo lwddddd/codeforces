61d54bd7df7dc3ded2df06ad9c286222317a79ed
wyx <Turingwyx@163.com>
2016-10-24 13:09:01 +0800

    Canada Cup 2016
    A
    Jumping Ball

---
+++ A Jumping Ball.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+char cnt[200000];
+int  main() {
+	int n;
+	int sum=0,flag=1;
+	scanf("%d",&n);
+	for(int i=0; i<n; i++) {
+		cin>>cnt[i];
+	}
+	for(int i=0; i<n; i++) {
+		if(cnt[i]=='>' ){
+			sum=sum+i;
+			break;
+		}
+		if(i==(n-1) && cnt[i]!='>')
+			sum+=n;
+	} 
+	for(int i=n-1; i>=0; i--) {
+		if(cnt[i]=='<'){
+			sum=sum+(n-1)-i;
+			break;
+		}
+		if(i==0 && cnt[i]!='<')
+			sum+=n;
+	}
+	printf("%d",sum);
+	return 0;
+} 
